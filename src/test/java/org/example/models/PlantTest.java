102bbd4fdf6e1a18a91d6b5785fa8c7903592953
Ulf Bengtsson <ulf.bengtsson@lexicon.se>
2020-07-08 11:52:33 +0200

    generated some tests for Plant

---
+++ src/test/java/org/example/models/PlantTest.java
@@ -0,0 +1,27 @@
+package org.example.models;
+
+import junit.framework.TestCase;
+
+public class PlantTest extends TestCase {
+
+    public void testHowDangerous() {
+    }
+
+    public void testTestGetName() {
+    }
+
+    public void testTestSetName() {
+    }
+
+    public void testGetRootType() {
+    }
+
+    public void testSetRootType() {
+    }
+
+    public void testGetPoiseLevel() {
+    }
+
+    public void testSetPoiseLevel() {
+    }
+}
